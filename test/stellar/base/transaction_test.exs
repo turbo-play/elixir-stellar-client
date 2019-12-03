362e22842f15126b429ca495497474a52c1e9ae2
Francisco Molina <francisco.molina@kommit.co>
2019-12-03 10:32:51 -0500

    Add tests for SetOptions operation

--- test/stellar/base/transaction_test.exs
+++ test/stellar/base/transaction_test.exs
@@ -35,4 +35,32 @@ defmodule Stellar.Base.Transaction.Test do
     verified = signer |> KeyPair.verify(signed_transaction |> Transaction.hash(), raw_sig_details)
     assert verified == true
   end
+
+  test "signs set_options correctly" do
+    source =
+      Account.new("GDRSG4KRN6SFM3C7NFRVB5Y3PR6OFEBY4TOP4EHLAAMZXRAWJMRBO4VE", 29_888_677_412_932)
+
+    signer = KeyPair.from_secret("SDHPVJCQEFM5CJ4NDZYGZYOG3DXV35QHR5IQO3VR3BT2YTS2U3DZCJMB")
+
+    {:ok, transaction, _} =
+      TransactionBuilder.new(source, [{:fee, 100}])
+      |> TransactionBuilder.add_operation(
+        Operation.set_options(%{
+          master_weight: 3
+        })
+      )
+      |> TransactionBuilder.set_timeout(10)
+      |> TransactionBuilder.build()
+
+    signed_transaction =
+      transaction
+      |> Transaction.sign(signer)
+
+    env = signed_transaction |> Transaction.to_envelope()
+
+    raw_sig = env.signatures |> Enum.at(0)
+    raw_sig_details = raw_sig.signature
+    verified = signer |> KeyPair.verify(signed_transaction |> Transaction.hash(), raw_sig_details)
+    assert verified == true
+  end
 end
